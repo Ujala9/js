0e15ae405339ac20ccb4ceff4e4c254ca4304541
Ujala9 <154305801+Ujala9@users.noreply.github.com>
2024-11-15 03:29:24 +0000

    container conigue done

---
+++ 01_Basics/01_function.js
@@ -0,0 +1,10 @@
+//functions means package of some lines of code where we can use as many time we want
+
+function sayMyName (){
+    console.log("U")
+    console.log("j")
+    console.log("al")
+    console.log("a")
+}
+
+sayMyName()//function call
